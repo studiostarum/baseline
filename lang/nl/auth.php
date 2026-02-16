22df5a35a8f29e91eb8aa101f42ffca47cb3927f
Studio Starum <studio.starum@gmail.com>
2026-02-16 14:09:27 +0100

    chore: add auth validation messages (failed, throttle)

---
+++ lang/nl/auth.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'failed' => 'Deze inloggegevens komen niet overeen met onze gegevens.',
+    'throttle' => 'Te veel inlogpogingen. Probeer het over :seconds seconden opnieuw.',
+];
